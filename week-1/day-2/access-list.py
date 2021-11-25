6ac22ac1370cd956b7640999f29f65b433823cfe
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 17:23:49 +0530

    python: list access elements

---
+++ week-1/day-2/access-list.py
@@ -0,0 +1,8 @@
+def list_elements() -> None:
+    days = [2, 6, 5, 7, 9, 10, 23, 45]
+    print(days[2:4:1])
+    print(days[6])
+    print(days[1:6:2])
+
+
+list_elements()
---
+++ week-1/day-2/basics-list.py
@@ -0,0 +1,8 @@
+num = [2, 4, 8, 10]
+days = ['sun', 'mon', 'tue']
+bools = [True, False, True]
+decimals = [12.63, 78.41, 96.52]
+
+print(isinstance(num, list))
+
+
