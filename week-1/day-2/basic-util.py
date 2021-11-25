6a9c24fc5d8820e4028016670b66138b2a354338
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 12:51:20 +0530

    python: isinstance use case

---
+++ week-1/day-2/basic-util.py
@@ -0,0 +1,15 @@
+
+def convertToFormat(raw):
+    assert isinstance(raw, str)
+    print('converting our format : ', raw)
+
+
+def check_type():
+    num1 = 10
+    num1 = 'abc'
+
+    print(isinstance(num1, int))
+
+
+convertToFormat('android')
+convertToFormat(100)
