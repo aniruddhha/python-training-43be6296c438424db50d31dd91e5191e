fd337a152895a428192ff0896274ed5203dbb933
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 14:17:03 +0530

    python: string basics

---
+++ week-1/day-2/str-ops.py
@@ -0,0 +1,18 @@
+def basics():
+    dt = 'android'
+    bt = "android 't' "
+    print(dt, bt)
+
+
+def multi_line():
+    ver = 10
+
+    dt = ''' 
+        This is android with version {ver}
+        with varibles will also work.
+     '''
+
+    print(dt)
+
+
+multi_line()
