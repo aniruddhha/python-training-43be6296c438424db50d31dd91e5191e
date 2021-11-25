f2e7865c10d538661f1ab03b9685fb6a3b94f6b1
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 15:01:12 +0530

    python: string slicing

--- week-1/day-2/str-access-ops.py
+++ week-1/day-2/str-access-ops.py
@@ -7,15 +7,16 @@ def elements():
 
 def slice_it():
     os = 'android'
-    print('-1', os[-1])
-    print('-2', os[-2])
-    print('-3', os[-3])
-    print('-4', os[-4])
-    print('-5', os[-5])
-    print('-6', os[-6])
+    print('-1', os[-1])  # d
+    print('-2', os[-2])  # i
+    print('-3', os[-3])  # o
+    print('-4', os[-4])  # r
+    print('-5', os[-5])  # d
+    print('-6', os[-6])  # n
+    print('-7', os[-7])  # a
 
     print('[1:3]', os[1:3])
-    print('[-1:-3]', os[-3:-1])
+    print('[-3:-1]', os[-3:-1])
     print('[:4]', os[:4])
     print('[3:]', os[3:])
 
