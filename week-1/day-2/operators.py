3ba2dece75eacdb2235bc521ad20a137c91a8946
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 12:02:37 +0530

    python: arithmatic operators

--- week-1/day-2/operators.py
+++ week-1/day-2/operators.py
@@ -1,2 +1,17 @@
 ''' Operators in python '''
 
+num1 = 10
+num2 = 20
+
+
+def arithmatic():
+    print('Addition is : ', num1 + num2)
+    print('Subtraction is : ', num1 - num2)
+    print('Multiplication is : ', num1 * num2)
+    print('Division is : ', num1 / num2)
+    print('Exponent : ', num1 ** 2)
+    print('Floor Division : ', num1 // 2)
+    print('Modulas : ', num1 % 2)
+
+
+arithmatic()
