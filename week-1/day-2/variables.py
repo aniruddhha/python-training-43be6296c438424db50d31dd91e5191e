42fa536fe49e688c319985539572568966e48a16
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 09:43:45 +0530

    python: variables

---
+++ week-1/day-2/variables.py
@@ -0,0 +1,11 @@
+''' Variables and Its types '''
+
+age = 10
+name = 'android'
+price = 45.56
+isStock = True
+
+print('Age is ', age)
+print('Name is ', name)
+print('Price is ', price)
+print('Stock OS ', isStock)
