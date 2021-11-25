28e8589759e2cb772c68c6e44bb0e085b6608097
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-25 10:09:31 +0530

    python : methods in details

--- week-1/day-2/method.py
+++ week-1/day-2/method.py
@@ -1,14 +1,27 @@
 ''' Methods/Functions and its definations '''
 
 
-def info(): {
+def info():
     print('hellow')
-}
 
 
 def info_details():
     print('here are machine details')
 
 
+def info_with_parameter(isSql, con):
+    print('Is SQL Db : ', isSql)
+    print('Connection : ', con)
+
+
+def info_with_return():
+    print('well I am returning')
+    return 20
+
+
 info()
 info_details()
+info_with_parameter(False, "http://abc.com")
+
+ret = info_with_return()
+print('Return Value is ', ret)
