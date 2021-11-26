b40878de87afc9701023a255b8f04bfd532a58af
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 09:33:41 +0530

    python: list operations

--- week-1/day-3/list-ops-cntd.py
+++ week-1/day-3/list-ops-cntd.py
@@ -2,17 +2,31 @@
 data = []
 
 
-def add() -> None:
-    print('adding')
+def add(dt) -> None:
+    data.append(dt)
 
 
-def remove() -> None:
-    print('removing')
+def remove(dt) -> None:
+    data.remove(dt)
 
 
-def update() -> None:
-    print('updating')
+def update(n, dt) -> None:
+    data.insert(n, dt)
 
 
 def display() -> None:
-    print('displaying')
+    print(data)
+
+
+def displayNth(n) -> None:
+    print(data[n])
+
+
+add(10)
+display()
+add(20)
+display()
+update(1, 30)
+display()
+remove(10)
+display()
