179d632e0c647e6fe43067c3c83fa31c8a4d9bdb
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 14:31:52 +0530

    python : unpacking

---
+++ week-1/day-3/unpacking-variables.py
@@ -0,0 +1,13 @@
+def unpacking_sequence():
+    names = ['abc', 'pqr', 'lmn']
+    print(names)
+    print(*names)
+    names = ('abc', 'pqr', 'lmn')
+    print(names)
+    print(*names)
+
+    # print(type(names))
+    # print(type(*names))
+
+
+unpacking_sequence()
