0c61a1adc2d8c9092236e243b5727742df530930
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 14:43:46 +0530

    python: upacking use case

--- week-1/day-3/unpacking-variables.py
+++ week-1/day-3/unpacking-variables.py
@@ -1,3 +1,9 @@
+
+def sample_runner(start, stop, step):
+    for i in range(start, stop, step):
+        print(i)
+
+
 def unpacking_sequence():
     names = ['abc', 'pqr', 'lmn']
     print(names)
@@ -11,8 +17,15 @@ def unpacking_sequence():
     print(names)
     print(*names)
 
+    lp_dtl = {1, 10, 2}
+    sample_runner(*lp_dtl)
+
     # print(type(names))
     # print(type(*names))
+
+
+def unpacking_dictionary():
+    pass
 
 
 unpacking_sequence()
