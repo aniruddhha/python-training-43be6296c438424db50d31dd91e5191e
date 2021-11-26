d1b9cb0c4ea3e99bc9027c458b6438e3e5230c61
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 17:55:02 +0530

    week 1 : assignments

--- week-1/assigments.py
+++ week-1/assigments.py
@@ -17,3 +17,52 @@
 **
 *
 '''
+
+# 5. Write a function to calculate the sum, avg, min, max of given list
+
+# 6. Write a function to calculate second highest element in a list
+
+# 7. Write a function to calculate nth highest element in a list
+
+# 8. Get marks from user and display result conditions, marks > 70 = distinction, marks > 60 = pass, marks <= 60 second class, marks >= 40 third class, marks < 40 = fail
+
+# 9. Get investment amount from user and suggest stock names to invest in. Amount > 1L = abc, amount between 1.5 to 2L = pqr, amount between 2.5L and above = xyz
+
+# 10.Write a function to print string array in reversed order  (use loop, no reverse/ready function)
+
+# 11. Write a function merge to 2 integer lists. input 2 lists, output merged list
+
+# 12. Write a function to find common elemnts from 2 sets. input 2 arrays, output set of comman elements
+
+# 13. Write a function to find unique elements from 2 sets. input 2 arrays, output set of unique elements
+
+# 14. Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+# 15. Write a program which can compute the factorial of a given numbers.The results should be printed in a comma-separated sequence on a single line.Suppose the following input is supplied to the program: 8 Then, the output should be:40320
+
+# 16. With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary.Suppose the following input is supplied to the program: 8
+# Then, the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+# 17. Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number.Suppose the following input is supplied to the program: 34,67,55,33,12,98
+
+# 18. Write a program that calculates and prints the value according to the given formula:
+# Q = Square root of [(2 _ C _ D)/H]
+# Following are the fixed values of C and H:
+# C is 50. H is 30.
+# D is the variable whose values should be input to your program in a comma-separated sequence.For example Let us assume the following comma separated input sequence is given to the program:
+# 100,150,180
+# The output of the program should be: 18,22,24
+
+# 19. Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+# Then, the output should be:
+# bag,hello,without,world
+
+# 20. Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+# Suppose the following input is supplied to the program:
+# Hello world
+# Practice makes perfect
+# Then, the output should be:
+# HELLO WORLD
+# PRACTICE MAKES PERFECT
