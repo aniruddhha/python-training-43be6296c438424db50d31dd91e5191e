27a09a6ad1f81e529d16a27fbaad96e22dbe10ec
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 15:22:24 +0530

    python: exception basics

---
+++ week-1/day-3/exceptions-basics.py
@@ -0,0 +1,18 @@
+def calculate(num1, num2):
+    return num1/num2
+
+
+def list_prop(n):
+    names = [10, 20, 30]
+    print(names[n])
+
+
+print(f'Result is {calculate(10, 20)}')
+print(f'Result is {calculate(50, 96)}')
+print(f'Result is {calculate(1, 1)}')
+# print(f'Result is {calculate(1, 0)}')
+print(f'Result is {calculate(0, 1)}')
+print('welcome to python')
+
+list_prop(1)
+list_prop(10)
