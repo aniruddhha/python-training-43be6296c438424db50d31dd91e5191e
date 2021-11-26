e474e6fd7d4fbaaa29fb9d40b2d76d73c882c9fb
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 15:34:18 +0530

    python : exception handling

--- week-1/day-3/exceptions-basics.py
+++ week-1/day-3/exceptions-basics.py
@@ -1,5 +1,28 @@
-def calculate(num1, num2):
-    return num1/num2
+def calculate(num1, num2, n):
+    names = [10, 20, 30]
+
+    try:
+        res = num1/num2 + names[n]
+
+    except ZeroDivisionError:
+        res = 0
+
+    except IndexError:
+        res = -1
+
+    return res
+
+
+def calculate_with_n(num1, num2, n):
+    names = [10, 20, 30]
+
+    try:
+        res = num1/num2 + names[n]
+
+    except (ZeroDivisionError, IndexError):
+        res = 0
+
+    return res
 
 
 def list_prop(n):
@@ -7,12 +30,12 @@ def list_prop(n):
     print(names[n])
 
 
-print(f'Result is {calculate(10, 20)}')
-print(f'Result is {calculate(50, 96)}')
-print(f'Result is {calculate(1, 1)}')
-# print(f'Result is {calculate(1, 0)}')
-print(f'Result is {calculate(0, 1)}')
+print(f'Result is {calculate(10, 20, 1)}')
+print(f'Result is {calculate(50, 96, 0)}')
+print(f'Result is {calculate(1, 1, 0)}')
+print(f'Result is {calculate(1, 0, 1)}')
+print(f'Result is {calculate(0, 1, 10)}')
 print('welcome to python')
 
-list_prop(1)
-list_prop(10)
+# list_prop(1)
+# list_prop(10)
