d53136504a8a8f8bdfa28d04c249f15089b5c433
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-26 15:46:38 +0530

    python : exception raising

---
+++ week-1/day-3/exception-own.py
@@ -0,0 +1,21 @@
+
+'''
+    - start should be greater than 5
+    - stop should be less that 50
+    - only 2 steps are possible 1 and 2
+'''
+
+
+def sample_runner(start, stop, step):
+    if start < 5:
+        raise Exception(f'Invalid Start : {start}')
+    if stop > 50:
+        raise Exception(f'Invalid Stop : {stop}')
+    if step not in [1, 2]:
+        Exception(f'Invalid Step : {step}')
+
+    for i in range(start, stop, step):
+        pass
+
+
+sample_runner(1, 10, 1)
