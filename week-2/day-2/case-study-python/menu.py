5cc8e5af395678ec1da35fa94981713a415c7a97
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-30 09:53:43 +0530

    python : case-study : menu setup

--- week-2/day-2/case-study-python/menu.py
+++ week-2/day-2/case-study-python/menu.py
@@ -4,4 +4,37 @@ Responsible for displaying system menu.
 
 
 class Menu:
-    pass
+
+    def display_menu():
+
+        mn_role = '''
+            \n Please specify your role
+            1. Admin
+            2. User
+            3. Exit
+        '''
+
+        mn_user = '''
+            \n Enter you choice : 
+            1. Display Balance
+            2. Transfer
+            3. Withdraw
+            4. Deposit
+            5. Exit
+        
+        '''
+
+        mn_admin = '''
+            \n Enter you choice : 
+            1. Create Account
+            2. Display Balance
+            2. Transfer
+            3. Withdraw
+            4. Deposit
+            5. Status
+            6. Exit
+        '''
+        print()
+
+    def user_input():
+        pass
