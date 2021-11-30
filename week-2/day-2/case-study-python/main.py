c23fad69bbeb3bd7991fd40c843ef77982774f1c
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-11-30 10:20:53 +0530

    python : case-study : menu implementation

--- week-2/day-2/case-study-python/main.py
+++ week-2/day-2/case-study-python/main.py
@@ -1,4 +1,5 @@
-
+from menu import Menu
 
 if __name__ == '__main__':
-    pass
+    menu: Menu = Menu()
+    menu.cli()
--- week-2/day-2/case-study-python/menu.py
+++ week-2/day-2/case-study-python/menu.py
@@ -5,27 +5,27 @@ Responsible for displaying system menu.
 
 class Menu:
 
-    def display_menu():
-
-        mn_role = '''
-            \n Please specify your role
-            1. Admin
-            2. User
-            3. Exit
-        '''
-
-        mn_user = '''
-            \n Enter you choice : 
+    def display_user_menu(self) -> None:
+        print('''
+            \n --- User Menu --- : 
             1. Display Balance
             2. Transfer
             3. Withdraw
             4. Deposit
             5. Exit
-        
-        '''
+        ''')
 
-        mn_admin = '''
-            \n Enter you choice : 
+    def display_main_menu(self) -> None:
+        print('''
+            \n --- Main Menu ---
+            1. Admin
+            2. User
+            3. Exit
+        ''')
+
+    def display_admin_menu(self) -> None:
+        print('''
+            \n --- Admin Menu --- 
             1. Create Account
             2. Display Balance
             2. Transfer
@@ -33,8 +33,22 @@ class Menu:
             4. Deposit
             5. Status
             6. Exit
-        '''
-        print()
+        ''')
 
-    def user_input():
-        pass
+    def cli(self) -> None:
+        while True:
+            self.display_main_menu()
+            ch: int = int(input('Enter Your Choice : '))
+
+            if(ch == 1):
+                self.display_admin_menu()
+                ch: int = int(input('Enter Your Choice : '))
+                if(ch == 6):
+                    return
+            elif (ch == 2):
+                self.display_user_menu()
+                ch: int = int(input('Enter Your Choice : '))
+                if(ch == 5):
+                    return
+            else:
+                exit(0)
