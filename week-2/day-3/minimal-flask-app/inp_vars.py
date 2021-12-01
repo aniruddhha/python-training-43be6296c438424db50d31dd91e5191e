9336a89e48aa52a82cf13c59eab4be2d9fb98e24
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-01 17:07:13 +0530

    flask : input variables type

--- week-2/day-3/minimal-flask-app/inp_vars.py
+++ week-2/day-3/minimal-flask-app/inp_vars.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route('/myname/<nm>')
 def print_my_name(nm: str):
-    return f'<h1>Your name is {nm} </h1>'
+    return f'<h1>Your name is {escape(nm)} </h1>'
+
+
+@app.route('/addition/<int:num1>/<int:num2>')
+def addition(num1: int, num2: int):
+    return f'<h1> Addition {num1} and {num2} is {num1 + num2}</h1>'
