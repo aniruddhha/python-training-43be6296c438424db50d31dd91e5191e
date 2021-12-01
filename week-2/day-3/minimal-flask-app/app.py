e7b54948f611933ac96c5e63fcea431fd518e167
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-01 16:14:04 +0530

    flask : sanitizing the inputs

--- week-2/day-3/minimal-flask-app/app.py
+++ week-2/day-3/minimal-flask-app/app.py
@@ -1,8 +1,26 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")  # decorator
 def hello_world():
-    return "<p>Hello, World! Welcome to Flask</p>"
+
+    al = "<script>alert('hi')</script>"
+    return(
+        f'''
+            Hello, World! Welcome to Flask
+            {escape(al)}
+        '''
+    )
+
+
+@app.route("/abc")
+def hello_abc():
+    return "<p>Returninng ABC</p>"
+
+
+@app.route("/pqr")
+def hello_pqr():
+    return "<p>Returninng PWR</p>"
