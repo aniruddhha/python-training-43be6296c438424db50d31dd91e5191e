8ff4ba4aa93931552804494bab8252279d64df55
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-02 10:05:22 +0530

    flask : static files

--- week-2/day-4/tmpl_data/app.py
+++ week-2/day-4/tmpl_data/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', bg='static/bg.jpeg')
 
 
 @app.route('/myname')
