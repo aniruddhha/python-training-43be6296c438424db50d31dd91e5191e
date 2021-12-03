961ccfc5ae513432a6a23974883502c3ba8aea46
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-03 15:48:46 +0530

    flask : api post method

--- week-2/day-5/calculator-api/app.py
+++ week-2/day-5/calculator-api/app.py
@@ -11,3 +11,8 @@ def index():
 @app.route('/addition/<int:num1>/<int:num2>')
 def addition(num1: int, num2: int):
     return {'status': 'success', 'message': 'addition performed successfully', 'result': (num1 + num2)}
+
+
+@app.route('/addition_body', methods=['POST'])
+def addition_in_body():
+    return {'status': 'in progress', 'message': 'api is under development'}
