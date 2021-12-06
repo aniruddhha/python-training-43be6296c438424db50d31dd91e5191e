be42221c7aa4f00d4b5e4b9c24da7e142634e8ba
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-06 11:43:42 +0530

    flask : mysql create operation

--- week-3/mysql-flask-basics/app.py
+++ week-3/mysql-flask-basics/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from pymysql.connections import Connection
+from pymysql.cursors import Cursor
 from db.connectivity import Connectivity
 
 app = Flask(__name__)
 
 connectivity = Connectivity()
-db = connectivity.db
+db: Connection = connectivity.db
 
 
 @app.route('/')
@@ -14,4 +16,18 @@ def index():
         'apistatus': True,
         'dbstatus': db is not None,
         'message': 'Database Connectivity and API Integration'
+    }
+
+
+@app.route('/emp', methods=['POST'])
+def create_employee():
+    emp: dict = request.json
+    print(emp)
+
+    csr: Cursor = db.cursor()
+    cnt = csr.execute('insert into emp_dt values(%s, %s)', emp)
+    return {
+        'sts': 'success',
+        'msg': 'employee saved successfully',
+        'res': cnt
     }
