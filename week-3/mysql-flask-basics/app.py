b81a84ad6b97b464a5d3dd0fb5c0ad2d8c4f9bf1
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-06 14:16:20 +0530

    flask : mysql setup

--- week-3/mysql-flask-basics/app.py
+++ week-3/mysql-flask-basics/app.py
@@ -25,10 +25,26 @@ def create_employee():
     print(emp)
 
     csr: Cursor = db.cursor()
-    cnt = csr.execute('insert into emp_dt values(%(id)s, %(nm)s)', emp)
+    cnt = csr.execute(
+        'insert into emp_dt values(%(id)s, %(nm)s)', emp)  # recommended
+    # cnt = csr.execute('insert into emp_dt values(%s, %s)', [*emp.values()])
     db.commit()
+    csr.close()  # good practice to close cursor after performing an operation
     return {
         'sts': 'success',
         'msg': 'employee saved successfully',
         'res': cnt
+    }
+
+
+@app.route('/emp')
+def all_employees():
+    csr: Cursor = db.cursor()
+    csr.execute('select * from emp_dt')
+    rows = csr.fetchall()
+    csr.close()
+    return {
+        'msg': 'fetched data successfully',
+        'status': 'success',
+        'res': rows
     }
