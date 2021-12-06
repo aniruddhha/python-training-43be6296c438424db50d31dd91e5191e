02ad531d7f7ec01a3ed0085cc5b2bf1bf35f3ebf
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-06 10:48:44 +0530

    flask : mysql connectivity

---
+++ week-3/mysql-flask-basics/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from db.connectivity import Connectivity
+
+app = Flask(__name__)
+
+connectivity = Connectivity()
+db = connectivity.db
+print(db)
+
+
+@app.route('/')
+def index():
+
+    return {
+        'apistatus': True,
+        'dbstatus': db is not None,
+        'message': 'Database Connectivity and API Integration'
+    }
---
+++ week-3/mysql-flask-basics/db/connectivity.py
@@ -0,0 +1,17 @@
+import pymysql
+
+
+class Connectivity:
+    def __init__(self):
+        self.db = None
+        try:
+            self.db = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="password",
+                database="flaskdb",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        except:
+            print('Your credentials are wrong')
+            db = None
