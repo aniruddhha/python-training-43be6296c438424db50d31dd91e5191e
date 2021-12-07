89bb7cc733824a323f8bc8ea3c8b36a258a0692b
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-07 17:38:29 +0530

    flask : jwt setup before all requests

--- week-3/day-2/jwt-rest-api-security/api.py
+++ week-3/day-2/jwt-rest-api-security/api.py
@@ -4,7 +4,34 @@ import jwt
 
 app = Flask(__name__)
 
+
+@app.before_request
+def before_all_requests():
+    print('This is called')
+    print(request.headers)
+    print(type(request.headers))
+    print(request.headers.get('sdldkfjskldf'))
+    if('sdldkfjskldf' not in request.headers):
+        return {
+            'sts': 'unauthorized',
+            'msg': 'you need to pass token'
+        }, 401
+
+
+@app.after_request
+def after_all_requests(): pass
+
+
 api = Api(app)
+
+
+class Bank(Resource):
+    def get(self):
+        return {
+            'sts': 'success',
+            'msg': 'balance api',
+            'res': 200
+        }
 
 
 class AppUser(Resource):
@@ -12,7 +39,7 @@ class AppUser(Resource):
         user_dict = request.get_json()
 
         if(user_dict.get('user_name') == 'abc' and user_dict.get('password') == 'abc'):
-            secrete = '0123456789'
+            secrete = '0123456789sdfsdf'
             algorithm = 'HS256'
             return {
                 'sts': 'success',
@@ -27,6 +54,7 @@ class AppUser(Resource):
 
 
 api.add_resource(AppUser, '/auth')
+api.add_resource(Bank, '/bank')
 
 if __name__ == '__main__':
     app.run(debug=True)
