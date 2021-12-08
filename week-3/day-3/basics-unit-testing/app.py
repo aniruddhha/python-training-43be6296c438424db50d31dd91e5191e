fb2a513ed6f0b2cff0fa02830d44ffebeb9e36c2
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-08 10:43:36 +0530

    unit testing : basics of unit test

---
+++ week-3/day-3/basics-unit-testing/app.py
@@ -0,0 +1,9 @@
+'''
+- num should be greater than 10
+- result should be greater than 10
+- negative numbers are not allowed
+- zeros are not allowed
+- num1 should be less than num2
+'''
+def add(num1: int, num2: int) -> None:
+    return num1 + num2
