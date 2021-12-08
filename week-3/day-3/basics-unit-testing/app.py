c7124ae242712cf3dd14071a9bf71f086e4623ef
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-08 11:23:49 +0530

    unit testing : basics of unit test module

--- week-3/day-3/basics-unit-testing/app.py
+++ week-3/day-3/basics-unit-testing/app.py
@@ -5,5 +5,9 @@
 - zeros are not allowed
 - num1 should be less than num2
 '''
+
+
 def add(num1: int, num2: int) -> None:
-    return num1 + num2
+    if num1 > 10 and num2 > 10:
+        return num1 + num2
+    return -1
--- week-3/day-3/basics-unit-testing/apptest.py
+++ week-3/day-3/basics-unit-testing/apptest.py
@@ -1,2 +1,15 @@
 import unittest
+from app import add
 
+
+class TestAddition(unittest.TestCase):
+
+    def num_should_be_greater_than_ten(self):
+        num1 = 10
+        num2 = 10
+        res = add(num1, num2)
+        self.assertEquals(res, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
