3558a701798188c58c02bcc1591cc75214dbf6cf
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-09 17:52:24 +0530

    unit_testing : returning from mock methods

--- week-3/day-4/mock-testing/pg_mock_test.py
+++ week-3/day-4/mock-testing/pg_mock_test.py
@@ -15,7 +15,6 @@ mck_pg.transfer(455)
 mck_pg.deposit(410)
 mck_pg.withdraw(500)
 
-
 # mck_pg.balance(133)
 mck_pg.balance.assert_called()
 mck_pg.balance.assert_called_once()
@@ -26,3 +25,9 @@ print(f'Balance Arguments {mck_pg.balance.call_args}')
 print(f'Balance Count {mck_pg.balance.call_count}')
 print('---Method Calls On Mock Object---')
 print(mck_pg.method_calls)
+
+mck_pg.balance.return_value = 455
+dt = mck_pg.balance(133)
+# if condition is true then and only then, interpreter will pass to next line
+assert dt == 455
+print('Yes balance returned 455')
