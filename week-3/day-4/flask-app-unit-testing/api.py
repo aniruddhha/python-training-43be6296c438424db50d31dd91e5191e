7a8f9e07a461e84db128b0d2698b7dcf94f1ced5
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-09 11:53:49 +0530

    unit testing: flask rest api testing exceptions

--- week-3/day-4/flask-app-unit-testing/api.py
+++ week-3/day-4/flask-app-unit-testing/api.py
@@ -4,11 +4,32 @@
 '''
 
 from flask import Flask, request
+import datetime
 
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+
+def is_date_less_than_today(dt: str):
+
+    try:
+        date_obj = datetime.date.fromisoformat(dt)
+        today = datetime.date.today()
+        if date_obj >= today:
+            raise InvalidDateException('Date Should Be Less Than Today')
+
+    except ValueError:
+        raise
+
+    except InvalidDateException:
+        raise
+
+
+class InvalidDateException(Exception):
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
 
 
 class Hello(Resource):
@@ -21,11 +42,25 @@ class Hello(Resource):
 
     def post(self):
         obj = request.get_json()
-        return {
-            'sts': 'success',
-            'msg': 'os created successfully',
-            'res': obj
-        }, 201
+        dt = obj.get('date')
+        try:
+            is_date_less_than_today(dt)
+            return {
+                'sts': 'success',
+                'msg': 'os created successfully',
+                'res': obj
+            }, 201
+
+        except ValueError:
+            return {
+                'sts': 'fail',
+                'msg': 'Date Format Should be (YYYY-MM-DD)',
+            }, 400
+        except InvalidDateException:
+            return {
+                'sts': 'fail',
+                'msg': 'Date Should be Less Than Today',
+            }, 400
 
 
 api.add_resource(Hello, '/hello')
--- week-3/day-4/flask-app-unit-testing/api_test.py
+++ week-3/day-4/flask-app-unit-testing/api_test.py
@@ -39,6 +39,58 @@ class HelloResourceTest(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertDictEqual(response.get_json(), expected)
 
+    def test_post_invalid_date(self):
+        obj = {
+            'os': 'android',
+            'version': 11,
+            'name':  'not-named',
+            'date': '2021-12-12'
+        }
+
+        expected = {
+            'sts': 'fail',
+            'msg': 'Date Should be Less Than Today',
+        }
+
+        response = self.client.post('/hello', json=obj)
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(response.get_json(), expected)
+
+    def test_post_valid_date(self):
+        obj = {
+            'os': 'android',
+            'version': 11,
+            'name':  'not-named',
+            'date': '2021-12-08'
+        }
+
+        expected = {
+            'sts': 'success',
+            'msg': 'os created successfully',
+            'res': obj
+        }
+
+        response = self.client.post('/hello', json=obj)
+        self.assertEqual(response.status_code, 201)
+        self.assertDictEqual(response.get_json(), expected)
+
+    def test_post_wrong_date_format(self):
+        obj = {
+            'os': 'android',
+            'version': 11,
+            'name':  'not-named',
+            'date': '08-01-2021'
+        }
+
+        expected = {
+            'sts': 'fail',
+            'msg': 'Date Format Should be (YYYY-MM-DD)'
+        }
+
+        response = self.client.post('/hello', json=obj)
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(response.get_json(), expected)
+
     def tearDown(self) -> None:
         pass
 
