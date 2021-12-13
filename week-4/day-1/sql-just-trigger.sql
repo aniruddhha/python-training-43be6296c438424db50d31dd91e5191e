8225fad63c0921eb4832bed8920352b6083d51f6
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-13 11:56:06 +0530

    mysql : insert and update trigger

--- week-4/day-1/sql-just-trigger.sql
+++ week-4/day-1/sql-just-trigger.sql
@@ -1,8 +1,10 @@
-drop trigger `ac_dtl_txn_trigger`;
+
+drop trigger `ac_dtl_txn_update_trigger`;
+drop trigger `ac_dtl_txn_insert_trigger`;
 
 delimiter $$
-create trigger `ac_dtl_txn_trigger` 
-after insert on `account_details` for each row 
+create trigger `ac_dtl_txn_update_trigger` 
+after update on `account_details` for each row 
 begin
 	declare dt date;
     declare last_txn_id integer;
@@ -18,4 +20,21 @@ begin
 end $$
 
 delimiter ;
+
+delimiter $$
+create trigger `ac_dtl_txn_insert_trigger` 
+after insert on `account_details` for each row 
+begin
+	declare dt date;
+    declare last_txn_id integer;
+    
+    set @dt := (select curdate());
+    set @last_txn_id := (select `txn_id` from `account_transactions` order by `txn_id` desc limit 1);
+    
+    if(@last_txn_id is Null ) then
+		set @last_txn_id := 1;
+    end if;
+    
+	insert into `account_transactions` values(@last_txn_id + 1, @dt, 2, NEW.`ac_amt`, NEW.`src_ac`);
+end $$
 
--- week-4/day-1/sql-triggers-2.sql
+++ week-4/day-1/sql-triggers-2.sql
@@ -3,8 +3,12 @@ SELECT * FROM `account_details`;
 SELECT * FROM `bank_account`;
 
 insert into `bank_account` values('123456789', 'abc', true);
-insert into `account_details` values(1, 9999, '123456789');
+insert into `account_details` values(2, 10001, '123456789');
 
 delete from `bank_account`;
+delete from `account_details`;
+delete from `account_transactions`;
 
 select curdate();
+
+select `txn_id` from `account_transactions` order by `txn_id` desc limit 1;
