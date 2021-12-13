f81357b0d7736be85211b8753e4598837c8a78f0
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-13 12:40:26 +0530

    mysql : stored procedure in and out parameters

--- week-4/day-1/sql-stored-procedure-basics.sql
+++ week-4/day-1/sql-stored-procedure-basics.sql
@@ -11,3 +11,35 @@ create procedure `show_me_date`()
 delimiter ;
 
 call `show_me_date`();
+-- -----------------------------------------------------------
+delimiter $$
+create procedure `show_me_max_date`(out dt date) -- here out variable will work as return data
+	begin
+		select curdate() into dt;
+    end $$
+
+delimiter ;
+
+
+set @mdt = '';
+call `show_me_max_date`(@mdt);
+select @mdt;
+-- -----------------------------------------------
+
+delimiter $$
+create procedure `date_diff`(in dt1 date, in dt2 date, out diff date) -- here out variable will work as return data
+	begin
+		
+    end $$
+    
+delimiter ;
+-- ------------------------------------------------
+ delimiter $$
+ create procedure `simple_proc`(in str varchar(10)) -- here out variable will work as return data
+	begin
+		select * from `bank_account` where `ac_nm` like str;
+    end $$
+delimiter ;
+
+call `simple_proc`('%c');
+-- ------------------------------------------------
