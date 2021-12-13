e77a9110e1e0f4d30cebb64ead79c71ba12b666b
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-13 14:44:31 +0530

    mysql: views

--- week-4/day-1/sql-views-basics.sql
+++ week-4/day-1/sql-views-basics.sql
@@ -5,3 +5,9 @@ inner join `account_details` `ad`
 on `ba`.`ac_num` = `ad`.`src_ac`;
 
 select * from `name_balance_view`;
+
+alter view `name_balance_view` as 
+select `ad`.`ac_amt`
+from `bank_account` `ba`
+inner join `account_details` `ad`
+on `ba`.`ac_num` = `ad`.`src_ac`;
