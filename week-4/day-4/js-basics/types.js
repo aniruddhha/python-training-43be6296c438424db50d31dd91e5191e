a516c2df26b85ae6acc284c0ad97c8104fdf3d6b
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-16 11:09:47 +0530

    h5c3js : js types and valus

---
+++ week-4/day-4/js-basics/types.js
@@ -0,0 +1,16 @@
+let nm = 'abc'
+nm = 63
+
+let age = 45
+let salary = 45.62
+let isOkay = true
+
+let names = ['abc', 'pqr', 'lmn']
+let obj = {
+    'keyNum': 10,
+    'keyBool': false,
+    'keyString': 'abc',
+    'keyFn': function () {
+        console.log(`function in object`)
+    }
+}
