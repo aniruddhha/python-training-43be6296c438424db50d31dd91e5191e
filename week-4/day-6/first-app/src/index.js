22d0a15d80dd8a73d0b7ad395e045f0660d37c97
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-18 14:25:16 +0530

    react : basics of jsx

--- week-4/day-6/first-app/src/index.js
+++ week-4/day-6/first-app/src/index.js
@@ -2,10 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
+const version = 17.01
 
-const dt = <div className='cnt'>
-  <h1> This is React App</h1>
-</div>
+
+const mobiles = ['abc', 'pqr', 'lmn', 'xyz', 'bcc']
+const liMobiles = []
+
+for (let i = 0; i < mobiles.length; i++) {
+  liMobiles.push(<li> {mobiles[i]} </li>)
+}
+
+const dt = (
+  <div className='cnt'>
+    <div>
+      <h1> This is React App</h1>
+    </div>
+    <div>
+      <p> React Version is {version} </p>
+      <p>{10 + 23}</p>
+      <p>{new Date().toString()}</p>
+    </div>
+    <div>
+      <ol>
+        {liMobiles}
+      </ol>
+    </div>
+  </div>
+)
 
 const root = document.getElementById('root')
 
