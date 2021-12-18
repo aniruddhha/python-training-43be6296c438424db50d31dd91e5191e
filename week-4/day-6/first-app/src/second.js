6eb4999d5cabf786588d3d9448fefd0e789a0339
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-18 16:10:51 +0530

    react : state management

--- week-4/day-6/first-app/src/second.js
+++ week-4/day-6/first-app/src/second.js
@@ -6,8 +6,8 @@ import { useState } from 'react' // react hook
 export default function Second() {
 
     const arr = useState('hello welcome to react')
-    const msg = arr[0]
-    const setMsg = arr[1]
+    const m = arr[0]
+    const fn = arr[1]
 
     const [msg, setMsg] = useState('hello welcome to react')
 
