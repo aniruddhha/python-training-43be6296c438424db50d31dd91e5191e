f2221c5fc0429ea7a021dff2a720d858fc8a11cf
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-18 12:56:50 +0530

    react : basics of react

--- week-4/day-6/first-app/src/index.js
+++ week-4/day-6/first-app/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 
-const dt = <h1> Welcome To React </h1>
+const dt = <div className='cnt'>
+  <h1> This is React App</h1>
+
+</div>
 const root = document.getElementById('root')
 
 ReactDOM.render(dt, root);
