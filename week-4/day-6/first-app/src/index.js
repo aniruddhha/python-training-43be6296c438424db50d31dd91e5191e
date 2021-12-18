a11b65d3875d97a5b6c39df2a2f37698c45e5e19
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-18 14:48:50 +0530

    react : basics of component

--- week-4/day-6/first-app/src/index.js
+++ week-4/day-6/first-app/src/index.js
@@ -4,7 +4,6 @@ import './index.css'
 
 const version = 17.01
 
-
 const mobiles = ['abc', 'pqr', 'lmn', 'xyz', 'bcc']
 const liMobiles = []
 
@@ -12,24 +11,37 @@ for (let i = 0; i < mobiles.length; i++) {
   liMobiles.push(<li> {mobiles[i]} </li>)
 }
 
-const dt = (
-  <div className='cnt'>
-    <div>
-      <h1> This is React App</h1>
+// react gives you much more elegant way
+// i.e. create components
+
+function First() { // it is called as component
+  const dt1 = (
+    <div className='cnt'>
+      <div>
+        <h1> This is React App</h1>
+      </div>
+      <div>
+        <p> React Version is {version} </p>
+        <p>{10 + 23}</p>
+        <p>{new Date().toString()}</p>
+      </div>
+      <div>
+        <ol>
+          {liMobiles}
+        </ol>
+      </div>
     </div>
-    <div>
-      <p> React Version is {version} </p>
-      <p>{10 + 23}</p>
-      <p>{new Date().toString()}</p>
-    </div>
-    <div>
-      <ol>
-        {liMobiles}
-      </ol>
-    </div>
-  </div>
-)
+  )
+
+  return dt1
+}
+
+function Second() {
+  const dt = <div></div>
+}
+
 
 const root = document.getElementById('root')
 
-ReactDOM.render(dt, root);
+// ReactDOM.render(First(), root);
+ReactDOM.render(<First />, root)
