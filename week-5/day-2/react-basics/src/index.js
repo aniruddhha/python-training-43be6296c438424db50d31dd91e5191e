861c995ff95805331b2fa129ee56d03157d6808c
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-21 18:07:13 +0530

    reactjs  : form validation

--- week-5/day-2/react-basics/src/index.js
+++ week-5/day-2/react-basics/src/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Car from './car'
 import DropDownDemo from './dropdowndemo';
-import SignupPage from './signup-form';
+import { SignupPage, SignupPageV2 } from './signup-form';
 
 const car = <Car color='I am a main prop' price='100'>
   <h1> I am a child prop </h1>
@@ -14,7 +14,7 @@ const car = <Car color='I am a main prop' price='100'>
 const dropdown = <DropDownDemo bxCl="black"></ DropDownDemo>
 
 ReactDOM.render(
-  <SignupPage />,
+  <SignupPageV2 />,
   document.getElementById('root')
 );
 
--- week-5/day-2/react-basics/src/signup-form.js
+++ week-5/day-2/react-basics/src/signup-form.js
@@ -2,7 +2,7 @@ import react from 'react'
 import './signup-form.css'
 import { useState } from 'react'
 
-function SignupPage() {
+export function SignupPage() {
 
     const [userName, setUserName] = useState()
     const [email, setEmail] = useState()
@@ -11,7 +11,7 @@ function SignupPage() {
 
     const onEmailChanged = ev => setEmail(ev.target.value)
     const onPasswordChanged = ev => setPassword(ev.target.value)
-    const onCheckChanged = ev => setCheckOut(ev.target.value)
+    const onCheckChanged = ev => setCheckOut(ev.target.checked)
 
     const onFormSubmit = e => {
         e.preventDefault()
@@ -46,4 +46,79 @@ function SignupPage() {
     )
 }
 
-export default SignupPage
+export function SignupPageV2() {
+
+    const [formData, setFormData] = useState()
+
+    const onUserNameChanged = ev => setFormData({ ...formData, userName: ev.target.value })
+    const onEmailChanged = ev => setFormData({ ...formData, email: ev.target.value })
+    const onPasswordChanged = ev => setFormData({ ...formData, password: ev.target.value })
+    const onCheckChanged = ev => setFormData({ ...formData, checkout: ev.target.checked })
+
+    const onFormSubmit = e => {
+        e.preventDefault()
+        console.log('form submitted')
+        console.log(formData)
+
+        fetch('http://localhost:5000/abc', formData)
+    }
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+    };
+
+    return (
+        <div className="container d-flex flex-row-reverse">
+            <form onSubmit={onFormSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label" >User Name</label>
+                    <input
+                        onChange={onUserNameChanged}
+                        type="text"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                        required
+                    />
+                    {
+                        formData && !formData.userName && <div className="form-text text-danger">Invalid username</div>
+                    }
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input
+                        onChange={onEmailChanged}
+                        type="email"
+                        className="form-control"
+                        aria-describedby="emailHelp"
+                        required
+                    />
+                    {formData && !formData.email && !validateEmail(formData.email) && <div className="form-text text-danger">Email Is Required</div>}
+                    {formData && (formData.email && !validateEmail(formData.email)) && <div className="form-text text-danger">Invalid Email</div>}
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input
+                        onChange={onPasswordChanged}
+                        type="password"
+                        className="form-control"
+                        required
+                    />
+                    {formData && !formData.password && <div className="form-text text-danger">Password required</div>}
+                </div>
+                <div className="mb-3 form-check">
+                    <input
+                        onChange={onCheckChanged}
+                        type="checkbox"
+                        className="form-check-input"
+                    />
+                    <label htmlFor="form-check-label">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
