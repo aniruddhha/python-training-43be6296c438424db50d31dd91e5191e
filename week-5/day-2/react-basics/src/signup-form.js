693920edf21938e23574125b2013b6cb7370f85e
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-21 15:59:51 +0530

    react : form submission

--- week-5/day-2/react-basics/src/signup-form.js
+++ week-5/day-2/react-basics/src/signup-form.js
@@ -1,26 +1,43 @@
 import react from 'react'
 import './signup-form.css'
+import { useState } from 'react'
 
 function SignupPage() {
+
+    const [userName, setUserName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [checkOut, setCheckOut] = useState()
+
+    const onEmailChanged = ev => setEmail(ev.target.value)
+    const onPasswordChanged = ev => setPassword(ev.target.value)
+    const onCheckChanged = ev => setCheckOut(ev.target.value)
+
+    const onFormSubmit = e => {
+        e.preventDefault()
+        console.log('form submitted')
+        console.log(` Username ${userName}, Email ${email}, Password ${password}, Check ${checkOut}`)
+    }
+
     return (
         <div className="container d-flex flex-row-reverse">
-            <form >
+            <form onSubmit={onFormSubmit}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">User Name</label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" />
+                    <input onChange={ev => setUserName(ev.target.value)} type="text" className="form-control" aria-describedby="emailHelp" />
                     <div className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" aria-describedby="emailHelp" />
+                    <input onChange={onEmailChanged} type="email" className="form-control" aria-describedby="emailHelp" />
                     <div className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" />
+                    <input onChange={onPasswordChanged} type="password" className="form-control" />
                 </div>
                 <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" />
+                    <input onChange={onCheckChanged} type="checkbox" className="form-check-input" />
                     <label htmlFor="form-check-label">Check me out</label>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
