bbacb478f974ad6e54e86f9874648c955aaab80a
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-22 09:56:48 +0530

    react : bank case study project seup

---
+++ week-5/day-3/bank-case-study-reactapp/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { UserRegistration } from './user/registration'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <UserRegistration />,
+  document.getElementById('root')
+);
+
