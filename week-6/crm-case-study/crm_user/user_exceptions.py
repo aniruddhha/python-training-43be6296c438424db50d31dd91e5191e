0d2822b03d15c4c3e9840f793e7d500a00cd376b
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-12-28 15:27:03 +0530

    case study crm : user login feature

--- week-6/crm-case-study/crm_user/user_exceptions.py
+++ week-6/crm-case-study/crm_user/user_exceptions.py
@@ -1,3 +1,4 @@
 class UserNotFoundException(Exception):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
+        self.msg = msg
--- week-6/crm-case-study/customer/customer_exceptions.py
+++ week-6/crm-case-study/customer/customer_exceptions.py
@@ -1,8 +1,10 @@
 class InvalidCustomerMobileException(Exception):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
+        self.msg = msg
 
 
 class InvalidCustomerNameException(Exception):
     def __init__(self, msg: str) -> None:
         super().__init__(msg)
+        self.msg = msg
